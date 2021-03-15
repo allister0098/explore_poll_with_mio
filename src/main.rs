65f9d8e0471437565b67775c987bbf1227f3f91d
shotaro <s1210098@gmail.com>
2021-03-15 23:50:37 +0900

    [add]epoll/kqueueのラッパーサンプル

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use mio::net::{TcpListener, TcpStream};
+use mio::{Events, Interest, Poll, Token};
+
+const SERVER: Token = Token(0);
+const CLIENT: Token = Token(1);
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let mut poll = Poll::new()?;
+    let mut events = Events::with_capacity(128);
+
+    let addr = "127.0.0.1:13265".parse()?;
+    let mut server = TcpListener::bind(addr)?;
+
+    poll.registry()
+        .register(&mut server, SERVER, Interest::READABLE | Interest::WRITABLE)?;
+
+    loop {
+        poll.poll(&mut events, None)?;
+
+        for event in events.iter() {
+            match event.token() {
+                SERVER => {
+                    let connection = server.accept();
+                    drop(connection);
+                }
+                CLIENT => {
+                    if event.is_writable() {
+                        // ブロック無しでソケットに書き込み可能
+                    }
+                    if event.is_readable() {
+                        // ブロックなしでソケットを読み込み可能
+                    }
+
+                    return Ok(());
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
 }
