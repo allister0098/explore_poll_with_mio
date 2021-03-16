fb3f82447b9e8e8fe0494919ef5253a0db05211d
shotaro <s1210098@gmail.com>
2021-03-16 23:55:59 +0900

    [modify]入力した文字をポーリングする形に修正途中

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
 use std::error::Error;
 
+use mio::event::Event;
 use mio::net::{TcpListener, TcpStream};
-use mio::{Events, Interest, Poll, Token};
+use mio::{Events, Interest, Poll, Token, Registry};
+use std::collections::HashMap;
+use std::io::{self, Read, Write};
+use std::str::from_utf8;
 
 const SERVER: Token = Token(0);
-const CLIENT: Token = Token(1);
+
+const DATA: &[u8] = b"Hello world\n";
 
 fn main() -> Result<(), Box<dyn Error>>{
     let mut poll = Poll::new()?;
     let mut events = Events::with_capacity(128);
 
-    let addr = "127.0.0.1:13265".parse()?;
+    let addr = "127.0.0.1:13265".parse().unwrap();
     let mut server = TcpListener::bind(addr)?;
 
     poll.registry()
-        .register(&mut server, SERVER, Interest::READABLE | Interest::WRITABLE)?;
+        .register(&mut server, SERVER, Interest::READABLE)?;
+
+    let mut connections = HashMap::new();
+    let mut unique_token = Token(SERVER.0 + 1);
+
+    println!("You can connect to the server using `nc`:");
+    println!(" $ nc 127.0.0.1 9000");
+    println!("You'll see our welcome message and anything you type we'll be printed here.");
 
     loop {
         poll.poll(&mut events, None)?;
@@ -27,10 +39,10 @@ fn main() -> Result<(), Box<dyn Error>>{
                 }
                 CLIENT => {
                     if event.is_writable() {
-                        // ブロック無しでソケットに書き込み可能
+                        println!("socket is writable");
                     }
                     if event.is_readable() {
-                        // ブロックなしでソケットを読み込み可能
+                        println!("socket is readable");
                     }
 
                     return Ok(());
